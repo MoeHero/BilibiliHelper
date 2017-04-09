1eed7828ce9a062bf70e2f96f83988952a289a8a
MoeHero <MoeHero@users.noreply.github.com>
2017-04-09 16:21:14 +0800

    清空仓库

--- src/bilibili_live_inject.js
+++
@@ -1,6 +0,0 @@
-/* global extensionID: false */
-window._server_callback = window.server_callback;
-window.server_callback = function(json) {
-    chrome.runtime.sendMessage(extensionID, json);
-    window._server_callback(json);
-};
