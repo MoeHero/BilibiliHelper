aa9afedd321b7dd60b53c995edad17cb5c819b32
MoeHero <MoeHero@users.noreply.github.com>
2017-05-03 00:11:03 +0800

    更新Readme 直播设置新增选项 直播设置面板top改为自动计算

--- README.md
+++ README.md
@@ -5,7 +5,25 @@
 [![GitHub release](https://img.shields.io/github/release/MoeHero/BilibiliHelper.svg)](https://github.com/MoeHero/BilibiliHelper/releases)
 [![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](https://github.com/MoeHero/BilibiliHelper/blob/master/LICENSE)
 
-### Bilibili助手
+## TODO List
+### 计划功能
+- [ ] 切换默认排行榜页面
+- [ ] 直播设置支持保存
+- [ ] 赠送礼物增强
+- [ ] 主站功能
 
+### 下版本预计功能
+- [ ] 领取瓜子后瓜子数量动态更新
+
+### 已完成功能
+- [x] 自动领取应援棒
+- [x] 自动领取小电视
+- [x] 礼物包裹增强
+- [x] 自动领瓜子
+- [x] 自动签到
+- [x] 直播设置
+- [x] 抽奖统计
+
+## License
 ※ 本项目使用 [Mozilla Public License 2.0](https://github.com/MoeHero/BilibiliHelper/blob/master/LICENSE) 许可协议  
 ※ 本项目部分代码参考自 [zacyu](https://github.com/zacyu) 和 [jjj201200](https://github.com/jjj201200) 以 [MIT License](https://github.com/zacyu/bilibili-helper/blob/master/LICENSE) 许可协议发布的开源项目 [bilibili-helper](https://github.com/zacyu/bilibili-helper)
--- src/bilibili_live/FuncLiveSetting.class.js
+++ src/bilibili_live/FuncLiveSetting.class.js
@@ -8,6 +8,7 @@ class FuncLiveSetting {
             vip: {name: '老爷进场', click: this.vip_click, state: true},
             sysmsg: {name: '系统公告', click: this.sysmsg_click, state: true},
             tvmsg: {name: '小电视公告', click: this.tvmsg_click, state: true},
+            'show-admin': {name: '显示房管列表', click: this.show_admin_click, state: false},
             'super-gift': {name: '礼物连击', click: this.super_gift_click, state: true}
         };
 
@@ -20,13 +21,16 @@ class FuncLiveSetting {
         this.liveSettingPanel = $('<div>').addClass('bh-live-setting-panel live-hover-panel arrow-bottom show').hide();
         this.liveSettingPanel.append($('<h4>').addClass('bh-title').text('直播设置')).append($('<hr>'));
         let ul = $('<ul>');
+        let top = -70;
         for(let key in this.funcList) {
             let li = $('<li>').addClass('clear-float');
             li.append($('<span>').text(this.funcList[key].name));
-            li.append($('<a>').addClass('bh-live-setting-btns link bili-link f-right').attr('key', key).text('隐藏'));
+            li.append($('<a>').addClass('bh-live-setting-btns link bili-link f-right').attr('key', key).text(this.funcList[key].state ? '隐藏' : '显示'));
             ul.append(li);
+            top += -20;
         }
-        this.liveSettingPanel.append(ul);
+        console.log(top);
+        this.liveSettingPanel.append(ul).css('top', top + 'px');
         $('.profile-ctrl').append(this.liveSettingPanel).append(this.liveSettingButton);
     }
     static addEvent() {
@@ -47,7 +51,7 @@ class FuncLiveSetting {
 
     static gift_click() {
         if(this.funcList['gift'].state) {
-            this.gift_css = Live.addStylesheetByText('.gift-msg{display:none !important;}');
+            this.gift_css = Live.addStylesheetByText('.gift-msg{display:none!important;}');
         } else {
             this.gift_css.remove();
             this.chatListScrollToBottom();
@@ -55,7 +59,7 @@ class FuncLiveSetting {
     }
     static vip_click() {
         if(this.funcList['vip'].state) {
-            this.vip_css = Live.addStylesheetByText('.system-msg{display:none !important;}');
+            this.vip_css = Live.addStylesheetByText('.system-msg{display:none!important;}');
         } else {
             this.vip_css.remove();
             this.chatListScrollToBottom();
@@ -63,7 +67,7 @@ class FuncLiveSetting {
     }
     static sysmsg_click() {
         if(this.funcList['sysmsg'].state) {
-            this.sysmsg_css = Live.addStylesheetByText('.sys-msg{display:none !important;}');
+            this.sysmsg_css = Live.addStylesheetByText('.sys-msg{display:none!important;}');
         } else {
             this.sysmsg_css.remove();
             this.chatListScrollToBottom();
@@ -71,10 +75,17 @@ class FuncLiveSetting {
     }
     static tvmsg_click() {
         if(this.funcList['tvmsg'].state) {
-            this.tvmsg_css = Live.addStylesheetByText('.small-tv-msg{display:none !important;}');
+            this.tvmsg_css = Live.addStylesheetByText('.small-tv-msg{display:none!important;}');
         } else {
             this.tvmsg_css.remove();
             this.chatListScrollToBottom();
+        }
+    }
+    static show_admin_click() {
+        if(this.funcList['show-admin'].state) {
+            this.show_admin_css.remove();
+        } else {
+            this.show_admin_css = Live.addStylesheetByText('.tab-switcher{width:59px!important;}.tab-switcher[data-type=room-admin]{width:62px!important;}');
         }
     }
     static super_gift_click() {
@@ -85,3 +96,4 @@ class FuncLiveSetting {
         $('#chat-msg-list').scrollTop($('#chat-msg-list')[0].scrollHeight);
     }
 }
+/**/
