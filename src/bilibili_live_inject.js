6f1e1070ed2469496710edb01d0942853c6e1f58
MoeHero <MoeHero@users.noreply.github.com>
2017-05-03 23:18:52 +0800

    删除冗余代码 修复图标问题

--- src/bilibili_live_inject.js
+++ src/bilibili_live_inject.js
@@ -6,11 +6,7 @@ window.server_callback = function(json) {
     chrome.runtime.sendMessage(extensionID, json);
 };
 //打开礼物包裹
-window._flash_giftPackageOpen = window.flash_giftPackageOpen;
-window.flash_giftPackageOpen = function() {
-    window._flash_giftPackageOpen();
-    chrome.runtime.sendMessage(extensionID, {command: 'openGiftPackage'});
-};
+window.flash_giftPackageOpen = () => chrome.runtime.sendMessage(extensionID, {command: 'openGiftPackage'});
 
 function bh_sendGift(giftID, number, bagID) {
     window.avalon.vmodels.giftPackageCtrl.$fire('all!sendGift', {
